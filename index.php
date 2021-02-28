e03ff5bf81ff5876f9b0240236bc518165fc7d31
Darkksinh <78621169+Darkksinh@users.noreply.github.com>
2021-02-28 05:48:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-$ curl -XGET https://br.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=boneca
-Response:
-{
-   Product: {
-      from: "",
-      title: "",
-      query: "",
-      pages: "",
-      missing: ""
-   }
-}
+<html>
+  <head>
+    <title>Custom Search JSON API Example</title>
+  </head>
+  <body>
+    <div id="content"></div>
+    <script>
+      function hndlr(response) {
+      for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        // in production code, item.htmlTitle should have the HTML entities escaped.
+        document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
+      }
+    }
+    </script>
+    <script src="https://www.googleapis.com/customsearch/v1?key=AIzaSyCcDWfGexWwNfup3ux_LGxPkw1ZDQ8nnGw&cx=017576662512468239146:omuauf_lfve&q=cars&callback=hndlr">
+    </script>
+  </body>
+</html>
